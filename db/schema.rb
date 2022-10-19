e50bfb8c686f95c42cd0b2b54165107e7ece8536
oszegda <oszegda@icloud.com>
2022-10-19 22:29:05 +0200

    articles migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_19_195630) do
+ActiveRecord::Schema.define(version: 2022_10_19_202456) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
